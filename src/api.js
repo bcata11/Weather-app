25ed5fc403295bdd85bec6e2a4153904fae9227d
bcata11 <catalinbar73@gmail.com>
2023-03-07 19:31:08 +0200

    first

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7500c2483cmsh5cac32a7cc98fb1p183fadjsn62c0c12448af',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "5d852961f328db102225c3876e26017b"
